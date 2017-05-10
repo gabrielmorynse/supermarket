0fbbf32ca1235a5161bf5dce6c3d98e1a3612b36
Fabricioti <fabricioti@gmail.com>
2017-05-10 10:50:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var itens;
+var item;
 var plataforms;
 var keys;
+var txtPontos;
+var pontos = 0;
 
 var Game = {
 
 	/*carregar os recurso durante o carregamento do jogo*/
 	preload: function() {
+		game.load.spritesheet('carrinho','image/carrinho.png',270, 249);
 		game.load.image('fundo','image/fundo.png');
 		game.load.image('piso','image/piso.png');
-		game.load.image('carrinho1','image/carrinho1.png');
-		game.load.image('carrinho2','image/carrinho2.png');
 		game.load.image('arroz','image/arroz.png');
 		game.load.image('banana','image/banana.png');
 		game.load.image('feijao','image/feijao.png');
 		game.load.image('leite','image/leite.png');
 		game.load.image('melancia','image/melancia.png');
 		game.load.image('prate','image/prate.png');
-
-		game.load.spritesheet('carrinho','image/carrinho.png',270, 249);
+		game.load.image('carFundo','image/carFundo.png');
 	},
 
 	/*criar os elementos que vão ser usados */
@@ -25,71 +26,87 @@ var Game = {
 		keys = game.input.keyboard.createCursorKeys();
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.add.sprite(0,0,'fundo');
-		
+	
 		plataforms = game.add.group();
 		plataforms.enableBody = true;
 
+		/* prateleria e piso para os itens*/
 		var piso = plataforms.create(0,game.world.height - 64,'piso');
+		piso.body.immovable = true;
 
-		/* prateleria para os itens*/
 		var prate = plataforms.create(97, 243, 'prate');
 		prate.body.immovable = true;
 
-		/*criar carrinho*/
-		var carrinho = game.add.sprite(50,game.world.height -250, 'carrinho');
-		game.physics.arcade.enable(carrinho);
-		carrinho.body.gravity.x = 90;
-
-		carrinho.body.collideWorldBounds = true;
-
+		var carFundo = plataforms.create(155, 10, 'carFundo');
+		carFundo.body.immovable = true;
+		carFundo.x = 90;
+		carFundo.y = 500;
 
 		itens = game.add.group();
 		itens.enableBody = true;
 
-		for(var i = 0; i <2; i++){
+		game.input.mouse.capture = true;
+
+
+		for(var i = 0; i <1; i++){
+				
+				var carrinho = game.add.sprite(10,game.world.height -50, 'carrinho');
+				game.physics.arcade.enable(carrinho);
+				carrinho.body.gravity.x = 0;
+				carrinho.body.collideWorldBounds = true;
+
 				var item = itens.create(i*2,0,'arroz');
 				item.x = 150, 300;
 				item.body.gravity.y = 50;
 				item.body.bounce.y = 0.2;
 				item.body.collideWorldBounds = true;
+				item.inputEnabled = true;
+    			item.input.enableDrag(true);
 
 				var item = itens.create(i*2,0,'banana');
 				item.x = 270;
 				item.body.gravity.y = 50;
 				item.body.bounce.y = 0.2;
 				item.body.collideWorldBounds = true;
+				item.inputEnabled = true;
+    			item.input.enableDrag(true);
 
 				var item = itens.create(i*2,0,'feijao');
 				item.x = 400;
 				item.body.gravity.y = 50;
 				item.body.bounce.y = 0.2;
 				item.body.collideWorldBounds = true;
+				item.inputEnabled = true;
+    			item.input.enableDrag(true);
 
 				var item = itens.create(i*2,0,'leite');
 				item.x = 550;
 				item.body.gravity.y = 50;
 				item.body.bounce.y = 0.2;
 				item.body.collideWorldBounds = true;
+				item.inputEnabled = true;
+    			item.input.enableDrag(true);
 
 				var item = itens.create(i*2,0,'melancia');
 				item.x = 670;
 				item.body.gravity.y = 50;
 				item.body.bounce.y = 0.2;
 				item.body.collideWorldBounds = true;
+				item.inputEnabled = true;
+    			item.input.enableDrag(true);
+
+    			txtPontos = game.add.text(10,10,'Pontos: 0',{fontSize:'18px', fill:'#fff'});
 		}
 	},
 
 	/* lógica do jogo a ser verificada, colisão, pulo, etc.*/
 	update: function(){
 		game.physics.arcade.collide(itens, plataforms);
-
-		
-		if(keys.left.isDown){
-			carrinho.body.velocity.x = -150;
-		} else
-		if(keys.right.isDown){
-			carrinho.body.velocity.x = 150;
-		}
+				
+		/*Captura click do mouse */
+		game.debug.text("Left Button: " + game.input.activePointer.leftButton.isDown, 300, 132);
+    	game.debug.text("Middle Button: " + game.input.activePointer.middleButton.isDown, 300, 196);
+    	game.debug.text("Right Button: " + game.input.activePointer.rightButton.isDown, 300, 260);
 
 	}
 
